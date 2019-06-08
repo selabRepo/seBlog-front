7321887a136e9c8fa589dd718c5fa9b36e4a9f4c
jeonyeonbin <qtrgor@gmail.com>
2019-06-08 17:42:20 +0900

    [STF-3] Blog Write 화면 변경 및 버튼 생성

--- components/BlogCategorySelect/BlogCategorySelect.jsx
+++ components/BlogCategorySelect/BlogCategorySelect.jsx
@@ -1,34 +1,4 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-
-
-const useStyles = makeStyles(theme => ({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-    },
-    formControl: {
-        margin: theme.spacing(1),
-        minWidth: 120,
-        maxWidth: 300,
-    },
-    chips: {
-        display: 'flex',
-        flexWrap: 'wrap',
-    },
-    chip: {
-        margin: 2,
-    },
-    noLabel: {
-        marginTop: theme.spacing(3),
-    },
-}));
-
 
 class BlogCategorySelect extends React.Component {
 
@@ -38,22 +8,17 @@ class BlogCategorySelect extends React.Component {
     }
 
     render() {
-
-        const names = ['abababababababa','bababbabba','c','d']
         return (
-            <div>
-                <FormControl>
-                    <InputLabel htmlFor="select-multiple">Name</InputLabel>
-                    <Select
-                        onChange={this.handleChange}
-                    >
-                        {names.map(name => (
-                            <MenuItem key={name} value={name}>
-                                {name}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                </FormControl>
+            <div className="d-flex justify-content-between" style={{marginTop : 20, marginBottom: 20}}>
+                <div className="p-2 col-example text-left">CATEGORY</div>
+
+                <select className="browser-default custom-select">
+                    <option>카테고리를 선택 해주세요.</option>
+                    <option value="java">java</option>
+                    <option value="javascript">javascript</option>
+                    <option value="database">database</option>
+                    <option value="react">React</option>
+                </select>
             </div>
         );
     }
