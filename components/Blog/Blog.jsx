0cb3a445f0fb578f697228539215f4fed731c57f
jyb5858 <jyb5858@nts-corp.com>
2019-08-03 14:56:56 +0900

    [STF-DEVELOP] conflict

--- components/Blog/Blog.jsx
+++ components/Blog/Blog.jsx
@@ -1,33 +1,16 @@
-<<<<<<< HEAD
-import Link from 'next/Link'
+import Link from 'next/link'
 import React from 'react'
+import BlogList from '../BlogList'
 
 const Blog = ({}) => {
   return (
     <div>
-      data
       <Link href={'/blog/write'}>
         <button>WriteButton Page</button>
       </Link>
+      <BlogList />
     </div>
   )
-=======
-import Link from "next/link";
-import React from "react";
-import BlogList from "../BlogList";
-
-const Blog = ({}) => {
-    return (
-        <div>
-            <Link href={"/blog/write"}>
-                <button>
-                    WriteButton Page
-                </button>
-            </Link>
-            <BlogList/>
-        </div>
-    )
->>>>>>> feature/STF-8-BlogList
 }
 
 export default Blog
