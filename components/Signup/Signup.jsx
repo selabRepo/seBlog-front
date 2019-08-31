667c539e662dd612f307454b23c01884c0ca96f1
youngjun.jin <yzzzzun@gmail.com>
2019-08-31 17:25:56 +0900

    [STF-10] 로그인 성공시 블로그 페이지로 이동 구현, 회원가입 컴포넌트 구성

---
+++ components/Signup/Signup.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Signup.scss';
+
+class Signup extends Component {
+    render() {
+        return (
+            <div>
+                회원가입    
+            </div>
+        );
+    }
+}
+
+export default Signup;
--- ducks/user.js
+++ ducks/user.js
@@ -1,30 +1,45 @@
 import axios from 'axios'
 import { createAction, handleActions } from 'redux-actions'
 import { pender } from 'redux-pender'
+import { fail } from 'assert';
 
 const initialState = {
     userId:null,
     jwtToken:null,
-    isLogin: false
+    isLogin: false,
+    isSuccess : false,
 }
 
+const USER_SIGNIN = 'user/USER_SIGNIN'
 const USER_SIGNUP = 'user/USER_SIGNUP'
 
-export const userSignup = createAction(USER_SIGNUP, params => {
+export const userSignin = createAction(USER_SIGNIN, params => {
   return axios.post("/api/login", params)
+})
+
+export const userSignup = createAction(USER_SIGNUP, params => {
+  return axios.post("/api/signup", params)
 })
 
 export default handleActions(
   {
     ...pender({
-      type: USER_SIGNUP,
+      type: USER_SIGNIN,
       onSuccess: (state, action) => {
         return {
           ...state,
           ...action.payload.data,
-          isLogin: true
+          isSuccess : true,
+          isLogin: true,
         }
       },
+      onFailure : (state, action) => {
+        return {
+          ...state,
+          isSuccess : false,
+          isLogin:false,
+        }
+      }
     }),
   },
   initialState,
