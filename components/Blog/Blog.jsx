85757756128f67d7c811478ef220296a89fadb44
jyb5858 <qtrgor@gmail.com>
2019-08-31 17:41:13 +0900

    [STF-16] BlogCategory add onclick event

--- components/Blog/Blog.jsx
+++ components/Blog/Blog.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import BlogList from '../BlogList'
 import './_Blog.scss'
+import BlogCategory from '../BlogCategory'
 
 const Blog = ({}) => {
   return (
-    <div className="Blog">
-      <BlogList />
-    </div>
+    <>
+      <div className="Blog">
+        <BlogList />
+        <BlogCategory />
+      </div>
+    </>
   )
 }
 
