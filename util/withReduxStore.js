29b71c35f9d905bb666a8b9c3f2ab0e4a58ddc82
jyb5858 <qtrgor@gmail.com>
2019-10-19 18:53:20 +0900

    [STF-23] ServerSide render done

---
+++ util/withReduxStore.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import store from '../store'
+
+const isServer = typeof window === 'undefined'
+const __NEXT_REDUX_STORE__ = '__NEXT_REDUX_STORE__'
+
+function getOrCreateStore(initialState) {
+  // Always make a new store if server, otherwise state is shared between requests
+  if (isServer) {
+    return store
+  }
+
+  // Create store if unavailable on the client and set it on the window object
+  if (!window[__NEXT_REDUX_STORE__]) {
+    window[__NEXT_REDUX_STORE__] = store
+  }
+  return window[__NEXT_REDUX_STORE__]
+}
+
+export default App => {
+  return class AppWithRedux extends React.Component {
+    static async getInitialProps(appContext) {
+      const reduxStore = getOrCreateStore()
+
+      appContext.ctx.reduxStore = reduxStore
+
+      let appProps = {}
+      if (typeof App.getInitialProps === 'function') {
+        appProps = await App.getInitialProps(appContext)
+      }
+
+      return {
+        ...appProps,
+        initialReduxState: reduxStore.getState(),
+      }
+    }
+
+    constructor(props) {
+      super(props)
+      this.reduxStore = getOrCreateStore()
+    }
+
+    render() {
+      return <App {...this.props} reduxStore={this.reduxStore} />
+    }
+  }
+}
